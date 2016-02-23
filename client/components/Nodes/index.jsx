c6cf7b7c71b1ac561325d842b1dfb9532937d089
Joshua Comeau <joshwcomeau@gmail.com>
2016-02-23 08:46:13 -0500

    Add Flip Move. Start trying to animate

--- client/components/Nodes/index.jsx
+++ client/components/Nodes/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component }     from 'react';
 import classNames               from 'classnames';
+import FlipMove                 from 'react-flip-move';
 
 import Node from './Node'
 
@@ -7,34 +8,36 @@ class Nodes extends Component {
   render() {
     return (
       <div id="nodes">
-        <div id="graveyard" className="nodes-region">
+        <FlipMove className="nodes-flip">
+          <div id="graveyard" className="nodes-region">
 
-        </div>
-        <div id="past" className="nodes-region">
-          <Node
-            key={this.props.nodes.get('past').get('name')}
-            data={this.props.nodes.get('past')}
-            clickNode={this.props.actions.clickNode}
-          />
-        </div>
-        <div id="present" className="nodes-region">
-          <Node
-            key={this.props.nodes.get('present').get('name')}
-            data={this.props.nodes.get('present')}
-            clickNode={this.props.actions.clickNode}
-          />
-        </div>
-        <div id="future" className="nodes-region">
-          {
-            this.props.nodes.get('future').map( node => (
-              <Node
-                key={node.get('name')}
-                data={node}
-                clickNode={this.props.actions.clickNode}
-              />
-            ))
-          }
-        </div>
+          </div>
+          <div id="past" className="nodes-region">
+            <Node
+              key={this.props.nodes.get('past').get('name')}
+              data={this.props.nodes.get('past')}
+              clickNode={this.props.actions.clickNode}
+            />
+          </div>
+          <div id="present" className="nodes-region">
+            <Node
+              key={this.props.nodes.get('present').get('name')}
+              data={this.props.nodes.get('present')}
+              clickNode={this.props.actions.clickNode}
+            />
+          </div>
+          <div id="future" className="nodes-region">
+            {
+              this.props.nodes.get('future').map( node => (
+                <Node
+                  key={node.get('name')}
+                  data={node}
+                  clickNode={this.props.actions.clickNode}
+                />
+              ))
+            }
+          </div>
+        </FlipMove>
       </div>
     );
   }
