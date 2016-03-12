63893ba202958217d4bdd400326de18f0963c737
Joshua Comeau <joshwcomeau@gmail.com>
2016-03-12 14:42:11 -0500

    Start animating groups of vertices

--- client/components/Graph/index.jsx
+++ client/components/Graph/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import { List, Map, fromJS } from 'immutable';
 
 import { repositionLength } from '../../config/timing';
 import { repositionEasing } from '../../config/easing';
@@ -7,15 +8,18 @@ class Graph extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      circle: { cx: 300, cy: 300, r: 100, fill: '#ABCDEF' },
-      line: { x1: 300, y1: 300, x2: 300, y2: 300, stroke: '#FFFFFF', 'stroke-width': 4 }
+      vertices: fromJS([
+        { id: 'a', cx: 600, cy: 150, r: 75},
+        { id: 'b', cx: 600, cy: 300, r: 75},
+        { id: 'c', cx: 600, cy: 450, r: 75}
+      ])
     };
   }
 
   moveTo(ev) {
-    const [ cx, cy ] = [ ev.clientX, ev.clientY ];
-    animateCircle(this.state.circle, this.setState.bind(this), cx, cy, 1000);
-    animateLine(this.state.line, this.setState.bind(this), cx, cy, 300, 300, 1000);
+    animateShapes(this.state, ::this.setState, {
+      vertices: this.state.vertices.map( v => v.update('cx', cx => cx + 25))
+    }, 1000)
   }
 
   render() {
@@ -23,64 +27,75 @@ class Graph extends Component {
 
     return (
       <svg id="graph" onClick={(ev) => this.moveTo(ev)}>
-        <line {...this.state.line} />
-        <circle {...this.state.circle} />
-
+        {
+          this.state.vertices.map( (v, i) => {
+            return (
+              <svg
+                key={i}
+                width={v.get('r') * 2}
+                height={v.get('r') * 2}
+                x={v.get('cx')}
+                y={v.get('cy')}
+              >
+                <circle
+                  cx="50%"
+                  cy="50%"
+                  r="50%"
+                  fill="#ABCDEF"
+                />
+                <text x="50%" y="50%" text-anchor="middle">Hello!</text>
+              </svg>
+            )
+          })
+        }
       </svg>
     );
   }
 };
 
-function animateCircle(circle, updater, cx, cy, duration) {
+function animateShapes(origin, setState, final, duration, easingFunction = easeInOutQuart) {
   const startTime = new Date().getTime();
-  const startingCx = circle.cx;
-  const startingCy = circle.cy;
 
   function updatePosition() {
     requestAnimationFrame( () => {
       const time = new Date().getTime() - startTime;
-      const newCx = easeInOutQuart(time, startingCx, cx - startingCx, duration);
-      const newCy = easeInOutQuart(time, startingCy, cy - startingCy, duration);
-      updater({
-        circle: { ...circle, cx: newCx, cy: newCy }
-      }, () => {
-        if ( time < duration ) updatePosition()
+
+      // Figure out the new center points for our vertices
+      const newVertices = origin.vertices.map( (vertex, vertexIndex) => {
+        // TODO: Replace these with .find calls, since we won't be guaranteed
+        // that the index is the same.
+        const originVertex  = origin.vertices.get(vertexIndex);
+        const finalVertex   = final.vertices.get(vertexIndex);
+
+        vertex = vertex
+          .set('cx', easingFunction(
+            time,
+            originVertex.get('cx'),
+            finalVertex.get('cx') - originVertex.get('cx'),
+            duration
+          ))
+          .set('cy', easingFunction(
+            time,
+            originVertex.get('cy'),
+            finalVertex.get('cy') - originVertex.get('cy'),
+            duration
+          ));
+
+        return vertex;
       });
-    })
+
+
+      setState({
+        vertices: newVertices
+      }, () => {
+        if ( time < duration ) updatePosition();
+      });
+    });
   }
 
   updatePosition();
 }
 
-function animateLine(line, updater, x1, y1, x2, y2, duration) {
-  console.log("Animating line to", x1, y1, x2, y2)
-  const startTime = new Date().getTime();
-  const startingX1 = line.x1;
-  const startingY1 = line.y1;
-  const startingX2 = line.x2;
-  const startingY2 = line.y2;
-
-  function updatePosition() {
-    requestAnimationFrame( () => {
-      const time = new Date().getTime() - startTime;
-      const newX1 = easeInOutQuart(time, startingX1, x1 - startingX1, duration);
-      const newY1 = easeInOutQuart(time, startingY1, y1 - startingY1, duration);
-      const newX2 = easeInOutQuart(time, startingX2, x2 - startingX2, duration);
-      const newY2 = easeInOutQuart(time, startingY2, y2 - startingY2, duration);
-      updater({
-        line: { ...line, x1: newX1, y1: newY1, x2: newX2, y2: newY2 }
-      }, () => {
-        if ( time < duration ) updatePosition()
-      });
-    })
-  }
-
-  updatePosition();
-}
-
-
-//
-// http://easings.net/#easeInOutQuart
 //  t: current time
 //  b: beginning value
 //  c: change in value
@@ -89,6 +104,12 @@ function animateLine(line, updater, x1, y1, x2, y2, duration) {
 function easeInOutQuart(t, b, c, d) {
   if ((t /= d / 2) < 1) return c / 2 * t * t * t * t + b;
   return -c / 2 * ((t -= 2) * t * t * t - 2) + b;
+}
+
+function linear(t, b, c, d) {
+  if ( t > d ) return b + c;
+  const ratio = t / d;
+  return b + (c * ratio);
 }
 
 
