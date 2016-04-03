2010fe6dea1855403c0ca00dfa0aea8822647594
Joshua Comeau <joshwcomeau@gmail.com>
2016-04-03 15:32:26 -0400

    Misc styling work

--- client/components/Footer/index.js
+++ client/components/Footer/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Footer = () => (
   <footer id="footer">
-    <h4>Made by Joshua Comeau. Powered by Spotify.</h4>
+    <h4>
+      Made by <a className="me" href="https://twitter.com/JoshWComeau">Joshua Comeau.</a> Powered by Spotify.
+    </h4>
     <h6>Copyright <span className="mono">new Date()</span>. All rights, wrongs, and grey areas reserved.</h6>
   </footer>
 );
