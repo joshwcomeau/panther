009d8a9eb90ddf8d10ffee7a2f5cf1d7b8a563f2
Joshua Comeau <joshwcomeau@gmail.com>
2016-04-03 08:38:03 -0400

    Add actions to fetch recent searches to Search duck

--- client/ducks/search.duck.js
+++ client/ducks/search.duck.js
@@ -1,14 +1,18 @@
-import { Map, List, fromJS } from 'immutable';
+import { Map, List, fromJS }    from 'immutable';
+
+import { fetchRecentSearches }  from '../helpers/api.helpers';
 
 
 ///////////////////////////
 // ACTION TYPES //////////
 /////////////////////////
-export const REQUEST_TYPEAHEAD_SUGGESTIONS = 'REQUEST_TYPEAHEAD_SUGGESTIONS';
-const RECEIVE_TYPEAHEAD_SUGGESTIONS = 'RECEIVE_TYPEAHEAD_SUGGESTIONS';
-const FAILURE_TYPEAHEAD_SUGGESTIONS = 'FAILURE_TYPEAHEAD_SUGGESTIONS';
-const CLEAR_TYPEAHEAD = 'CLEAR_TYPEAHEAD';
-const RECORD_VOICE = 'RECORD_VOICE';
+export const REQUEST_TYPEAHEAD_SUGGESTIONS  = 'REQUEST_TYPEAHEAD_SUGGESTIONS';
+export const RECEIVE_TYPEAHEAD_SUGGESTIONS  = 'RECEIVE_TYPEAHEAD_SUGGESTIONS';
+export const FAILURE_TYPEAHEAD_SUGGESTIONS  = 'FAILURE_TYPEAHEAD_SUGGESTIONS';
+export const REQUEST_RECENT_SEARCHES        = 'REQUEST_RECENT_SEARCHES';
+export const RECEIVE_RECENT_SEARCHES        = 'RECEIVE_RECENT_SEARCHES';
+export const FAILURE_RECENT_SEARCHES        = 'FAILURE_RECENT_SEARCHES';
+export const CLEAR_TYPEAHEAD                = 'CLEAR_TYPEAHEAD';
 
 
 ///////////////////////////
@@ -33,11 +37,14 @@ export default function reducer(state = initialState, action) {
     case FAILURE_TYPEAHEAD_SUGGESTIONS:
       return state;
 
+    case REQUEST_RECENT_SEARCHES:
+      return state;
+
+    case RECEIVE_RECENT_SEARCHES:
+      return state.set('recent', action.recent);
+
     case CLEAR_TYPEAHEAD:
       return initialState;
-
-    case RECORD_VOICE:
-      return state;
 
     default:
       return state;
@@ -48,7 +55,6 @@ export default function reducer(state = initialState, action) {
 ///////////////////////////
 // ACTION CREATORS ///////
 /////////////////////////
-
 export function requestTypeaheadSuggestions(term) {
   return {
     type: REQUEST_TYPEAHEAD_SUGGESTIONS,
@@ -70,15 +76,34 @@ export function failureTypeaheadSuggestions(error) {
   }
 }
 
+export function requestRecentSearches() {
+  return dispatch => {
+    // Start by dispatching this action.
+    // It's currently unused, but I may want to display something optimistic.
+    dispatch({ type: REQUEST_RECENT_SEARCHES });
+
+    fetchRecentSearches()
+      .then( recentSearches => dispatch(receiveRecentSearches(recentSearches)) )
+      .catch( err => dispatch(failureRecentSearches(err)) );
+  };
+}
+
+export function receiveRecentSearches(recent) {
+  return {
+    type: RECEIVE_RECENT_SEARCHES,
+    recent
+  };
+}
+
+export function failureRecentSearches(error) {
+  return {
+    type: FAILURE_RECENT_SEARCHES,
+    error
+  }
+}
+
 export function clearTypeahead() {
   return {
     type: CLEAR_TYPEAHEAD
   }
-}
-
-export function recordVoice() {
-  // TODO: Figure this out
-  return {
-    type: RECORD_VOICE
-  };
 }
